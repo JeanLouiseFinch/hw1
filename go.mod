50148ffaeeac47cb08214f168153926bdce45974
JeanLouiseFinch <JeanLouiseFinch00@protonmail.com>
2019-05-29 20:35:38 +0300

    ntp func

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/JeanLouiseFinch/hw1
+
+require (
+	github.com/beevik/ntp v0.2.0
+	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
+)
--- hw1.go
+++ hw1.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 	"io"
 	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const (
+	ntpServRu = "0.ru.pool.ntp.org"
 )
 
 // Now - печатаем время с помощью стандартной библиотеки
 func Now(out io.Writer) {
-	fmt.Fprintf(out, "%s\n", time.Now().Format(time.UnixDate))
+	fmt.Fprintf(out, "Системное время: %s\n", time.Now().Format(time.UnixDate))
+}
+
+//NowNTP - печатаем время по NTP серверу
+func NowNTP(host string, out io.Writer) {
+	if host == "" {
+		host = ntpServRu
+	}
+	timeNTP, err := ntp.Time(host)
+	if err != nil {
+		fmt.Fprintf(out, "Что-то пошло не так(%v). Системное время: %s\n", err, time.Now().Format(time.UnixDate))
+		return
+	}
+	fmt.Fprintf(out, "Время по NTP серверу: %s\n", timeNTP.Format(time.UnixDate))
 }
